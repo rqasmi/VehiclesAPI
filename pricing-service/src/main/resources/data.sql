71eb12ffd5eea8730ce3cd086d2f48f41674f7e8
Ragheed Qasmieh <ragheed.qasmieh@mail.mcgill.ca>
2021-06-21 17:02:17 -0400

    Initial commit.

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRICE (vehicle_id, currency, price) VALUES (1, 'USD', 2500);
+INSERT INTO PRICE (vehicle_id, currency, price) VALUES (2, 'USD', 8000.87);
+INSERT INTO PRICE (vehicle_id, currency, price) VALUES (3, 'CAD', 15000.42);
+INSERT INTO PRICE (vehicle_id, currency, price) VALUES (4, 'EUR', 20000);
